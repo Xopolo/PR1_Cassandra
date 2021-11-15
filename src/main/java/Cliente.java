a1bbd13129c87166c12f1b08cff8c85f4e99d5ff
Pablo Hernandez <phernandhy@alumnos.unex.es>
2021-11-15 19:41:25 +0100

    Initial commit

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,33 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+
+public class Cliente {
+
+    private Cluster cluster;
+    private Session session;
+
+    public Session getSession() {
+        return this.session;
+    }
+
+    public void connect(String node) {
+        cluster = Cluster.builder().addContactPoint(node).build();
+        Metadata metadata = cluster.getMetadata();
+        System.out.printf("Connected to cluster: %s\n",
+                metadata.getClusterName());
+        for (Host host : metadata.getAllHosts()) {
+            System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
+                    host.getDatacenter(), host.getAddress(), host.getRack());
+        }
+        session = cluster.connect();
+    }
+
+    public void close() {
+        session.close();
+        cluster.close();
+        System.out.println("Connection closed");
+    }
+
+}
---
+++ src/main/java/Main_test.java
@@ -0,0 +1,8 @@
+public class Main_test {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.connect("127.0.0.1");
+        cliente.close();
+
+    }
+}
